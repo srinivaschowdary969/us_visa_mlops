01c9470b42faaa7f7c9ee61247fcf9e2a485a9d2
srinivaschowdary969 <mechden143r@gmail.com>
2024-07-19 11:17:21 -0700

    setup updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_mlops",
+    version="0.0.0",
+    author="srinivas",
+    author_email="srinivas.bogineni13@gmail.com",
+    packages=find_packages()
+)
