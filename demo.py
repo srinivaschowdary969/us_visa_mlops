75496d03e2970b51ac59de6d5c5067dd7cd9cc2f
srinivaschowdary969 <mechden143r@gmail.com>
2024-07-20 03:35:43 -0700

    utils added

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
 from us_visa1.logger import logging
+from us_visa1.exception import USvisaException
+import os,sys
 
-logging.info("welcome to our log")
+#logging.info("welcome to our log")
+try:
+    a=2/0
+except Exception as e:
+    raise USvisaException(e,sys)
